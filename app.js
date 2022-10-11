70fee996b5eae7db1be256c2d147b789f31ae3b3
Oliwier Posiakow <oliwierposiek@gmail.com>
2022-10-11 15:53:20 +0200

    Added full HTML and JS, still working on CSS

---
+++ app.js
@@ -0,0 +1,53 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const char= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+let generateBtn = document.getElementById("generate-btn");
+let pswd1 = document.getElementById("pass-1");
+let pswd2 = document.getElementById("pass-2");
+
+generateBtn.addEventListener("click",function(){
+    let lengthEl = document.getElementById("length-in").value
+    let checkboxN = document.getElementById("if-numbers").checked;
+    let checkboxS = document.getElementById("if-special").checked;
+    let password1 = ""
+    let password2 = ""
+
+    if(checkboxN===true && checkboxS===true){
+        for(let i=0; i < lengthEl; i++){
+            let x = Math.floor(Math.random()*characters.length)
+            let y = Math.floor(Math.random()*characters.length)
+            password1 += characters[x]
+            password2 += characters[y]
+        }
+    }
+    else if(checkboxN === true && checkboxS === false){
+        for(let i=0; i < lengthEl; i++){
+            let x = Math.floor(Math.random()*(characters.length-29))
+            let y = Math.floor(Math.random()*(characters.length-29))
+            password1 += characters[x]
+            password2 += characters[y]
+        }
+    }
+    else if(checkboxN === false && checkboxS === true){
+        for(let i=0; i < lengthEl; i++){
+            let x = Math.floor(Math.random()*char.length)
+            let y = Math.floor(Math.random()*char.length)
+            password1 += char[x]
+            password2 += char[y]
+        }
+    }
+    else {
+        for(let i=0; i < lengthEl; i++){
+            let x = Math.floor(Math.random()*(characters.length-39))
+            let y = Math.floor(Math.random()*(characters.length-39))
+            password1 += characters[x]
+            password2 += characters[y]
+        }
+    }
+
+    pswd1.textContent = password1
+    pswd2.textContent = password2
+      
+}
+)
+    
